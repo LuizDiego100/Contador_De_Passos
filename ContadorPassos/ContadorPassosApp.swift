b0e91ce5505dbe3180246b6e9aa39b06368c55f0
Luiz <diegoo837ma@icloud.com>
2022-08-06 20:43:10 +0100

    Initial Commit

---
+++ ContadorPassos/ContadorPassosApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContadorPassosApp.swift
+//  ContadorPassos
+//
+//  Created by Luiz Andrade on 06/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct ContadorPassosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
